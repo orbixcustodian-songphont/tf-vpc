f8d27361062a0d82f256915f7033c5d0be7e950a
Jason Calalang <jcalalang@ph.ibm.com>
2025-01-06 17:08:50 +0800

    update packages

--- vsi-ansible.tf
+++ vsi-ansible.tf
@@ -4,7 +4,7 @@ resource ibm_is_instance ansible-vsi {
   image   = "r022-d5e7a447-981e-4ffe-906e-1ff648690bf9"
   zone    = "jp-tok-1"
   vpc     = ibm_is_vpc.vpc[0].id
-  depends_on = [ ibm_is_ssh_key.orbix_key, ibm_is_volume.home-vol ]
+  depends_on = [ ibm_is_ssh_key.orbix_key ]
   
   # Attach primary network interface
   primary_network_interface {
@@ -17,12 +17,15 @@ resource ibm_is_instance ansible-vsi {
   ]
 
   user_data = <<-EOT
-#cloud-config
-package_update: true
-packages:
-  - ansible-core
-runcmd:
-  - [ "/bin/bash", "-c", "set -e && dnf update -y && dnf install git ansible-core -y && ansible-galaxy collection install community.general ansible.posix" ]
+  #cloud-config
+  package_update: true
+  package_upgrade: true
+  packages:
+    - ansible-core
+  runcmd:
+    - [ "/bin/bash", "-c", "set -e && dnf update -y && dnf install git ansible-core -y && ansible-galaxy collection install community.general ansible.posix" ]
+    - [dnf, makecache]
+    - [dnf, -y, upgrade]
 EOT
 }
 
