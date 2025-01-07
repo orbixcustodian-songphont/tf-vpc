a22bbc10cebcc5545e55d8560bf054f3a3807045
Jason Calalang <jcalalang@ph.ibm.com>
2025-01-07 21:12:34 +0800

    add floating ip

--- fip.tf
+++ fip.tf
@@ -2,3 +2,8 @@ resource "ibm_is_floating_ip" "fip1" {
   name = "${var.vpc_name}-floating-ip"
   zone = "jp-tok-1"
 }
+
+resource "ibm_is_floating_ip" "fip3" {
+  name = "${var.vpc_name}-floating-ip-3"
+  zone = "jp-tok-3"
+}
