a490d91d7080328509a2a79391a42349e9a8b5c0
Jason Calalang <jcalalang@ph.ibm.com>
2025-01-09 23:01:48 +0800

    update volume

--- vsi-rhel.tf
+++ vsi-rhel.tf
@@ -1,55 +1,7 @@
-resource "ibm_is_volume" "rhel-home-vsi-vol" {
-  name     = "${var.vsi_name}-home-vsi-vol"
+resource "ibm_is_volume" "rhel-vsi-vol" {
+  name     = "${var.vsi_name}-vsi-vol"
   profile  = "general-purpose" # or "5iops-tier", "10iops-tier", etc.
-  capacity = 10                # in GB
-  zone     = "jp-tok-1"        # adjust to match your region/zone
-  resource_group = data.ibm_resource_group.resource-group-name.id
-}
-
-resource "ibm_is_volume" "rhel-tmp-vsi-vol" {
-  name     = "${var.vsi_name}-tmp-vsi-vol"
-  profile  = "general-purpose" # or "5iops-tier", "10iops-tier", etc.
-  capacity = 10                # in GB
-  zone     = "jp-tok-1"        # adjust to match your region/zone
-  resource_group = data.ibm_resource_group.resource-group-name.id
-}
-
-resource "ibm_is_volume" "rhel-var-vsi-vol" {
-  name     = "${var.vsi_name}-var-vsi-vol"
-  profile  = "general-purpose" # or "5iops-tier", "10iops-tier", etc.
-  capacity = 10                # in GB
-  zone     = "jp-tok-1"        # adjust to match your region/zone
-  resource_group = data.ibm_resource_group.resource-group-name.id
-}
-
-resource "ibm_is_volume" "rhel-var-log-vsi-vol" {
-  name     = "${var.vsi_name}-var-log-vsi-vol"
-  profile  = "general-purpose" # or "5iops-tier", "10iops-tier", etc.
-  capacity = 10                # in GB
-  zone     = "jp-tok-1"        # adjust to match your region/zone
-  resource_group = data.ibm_resource_group.resource-group-name.id
-}
-
-resource "ibm_is_volume" "rhel-var-log-audit-vsi-vol" {
-  name     = "${var.vsi_name}-var-log-audit-vsi-vol"
-  profile  = "general-purpose" # or "5iops-tier", "10iops-tier", etc.
-  capacity = 10                # in GB
-  zone     = "jp-tok-1"        # adjust to match your region/zone
-  resource_group = data.ibm_resource_group.resource-group-name.id
-}
-
-resource "ibm_is_volume" "rhel-var-tmp-vsi-vol" {
-  name     = "${var.vsi_name}-var-tmp-vsi-vol"
-  profile  = "general-purpose" # or "5iops-tier", "10iops-tier", etc.
-  capacity = 10                # in GB
-  zone     = "jp-tok-1"        # adjust to match your region/zone
-  resource_group = data.ibm_resource_group.resource-group-name.id
-}
-
-resource "ibm_is_volume" "rhel-dev-shm-audit-vsi-vol" {
-  name     = "${var.vsi_name}-dev-shm-audit-vsi-vol"
-  profile  = "general-purpose" # or "5iops-tier", "10iops-tier", etc.
-  capacity = 10                # in GB
+  capacity = 100               # in GB
   zone     = "jp-tok-1"        # adjust to match your region/zone
   resource_group = data.ibm_resource_group.resource-group-name.id
 }
@@ -65,12 +17,6 @@ resource "ibm_is_instance" "rhel-vsi" {
   depends_on = [
     ibm_is_ssh_key.ssh-key, 
     ibm_is_volume.rhel-home-vsi-vol, 
-    ibm_is_volume.rhel-tmp-vsi-vol, 
-    ibm_is_volume.rhel-var-vsi-vol, 
-    ibm_is_volume.rhel-var-log-vsi-vol, 
-    ibm_is_volume.rhel-var-log-audit-vsi-vol, 
-    ibm_is_volume.rhel-var-tmp-vsi-vol, 
-    ibm_is_volume.rhel-dev-shm-audit-vsi-vol
   ]
 
   # Attach primary network interface
@@ -86,42 +32,6 @@ resource "ibm_is_instance" "rhel-vsi" {
 
 resource "ibm_is_instance_volume_attachment" "rhel-home-attach" {
   instance                         = ibm_is_instance.rhel-vsi.id
-  volume                           = ibm_is_volume.rhel-home-vsi-vol.id
-  delete_volume_on_instance_delete = true
-}
-
-resource "ibm_is_instance_volume_attachment" "rhel-tmp-attach" {
-  instance                         = ibm_is_instance.rhel-vsi.id
-  volume                           = ibm_is_volume.rhel-tmp-vsi-vol.id
-  delete_volume_on_instance_delete = true
-}
-
-resource "ibm_is_instance_volume_attachment" "rhel-var-attach" {
-  instance                         = ibm_is_instance.rhel-vsi.id
-  volume                           = ibm_is_volume.rhel-var-vsi-vol.id
-  delete_volume_on_instance_delete = true
-}
-
-resource "ibm_is_instance_volume_attachment" "rhel-var-log-attach" {
-  instance                         = ibm_is_instance.rhel-vsi.id
-  volume                           = ibm_is_volume.rhel-var-log-vsi-vol.id
-  delete_volume_on_instance_delete = true
-}
-
-resource "ibm_is_instance_volume_attachment" "rhel-var-log-audit-attach" {
-  instance                         = ibm_is_instance.rhel-vsi.id
-  volume                           = ibm_is_volume.rhel-var-log-audit-vsi-vol.id
-  delete_volume_on_instance_delete = true
-}
-
-resource "ibm_is_instance_volume_attachment" "rhel-var-tmp-attach" {
-  instance                         = ibm_is_instance.rhel-vsi.id
-  volume                           = ibm_is_volume.rhel-var-tmp-vsi-vol.id
-  delete_volume_on_instance_delete = true
-}
-
-resource "ibm_is_instance_volume_attachment" "rhel-dev-shm-audit-attach" {
-  instance                         = ibm_is_instance.rhel-vsi.id
-  volume                           = ibm_is_volume.rhel-dev-shm-audit-vsi-vol.id
+  volume                           = ibm_is_volume.rhel-vsi-vol.id
   delete_volume_on_instance_delete = true
 }
