6b4beb88b0ae9e51365d61da20f5f941863807ae
Jason Calalang <jcalalang@gmail.com>
2025-01-09 21:46:17 +0800

    floating ip change

--- floating-ip.tf
+++ floating-ip.tf
@@ -1,4 +1,5 @@
 resource "ibm_is_floating_ip" "fip1" {
   name = "${var.vpc_name}-floating-ip"
   zone = "jp-tok-1"
+  resource_group = coalesce(data.ibm_resource_group.example.id,ibm_resource_group.example[0].id)
 }
