a3aaa86b9a735b990159dbd02940cd1b6b3e7449
Jason Calalang <jcalalang@gmail.com>
2025-01-09 21:53:23 +0800

    change name

--- floating-ip.tf
+++ floating-ip.tf
@@ -1,5 +1,5 @@
 resource "ibm_is_floating_ip" "fip1" {
   name = "${var.vpc_name}-floating-ip"
   zone = "jp-tok-1"
-  resource_group = coalesce(data.ibm_resource_group.example.id,ibm_resource_group.example[0].id)
+  resource_group = coalesce(data.ibm_resource_group.resource-group-name.id,ibm_resource_group.resource-group-name[0].id)
 }
