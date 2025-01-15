1c01d1cad68419ee52c095756eae76867a6ee9ee
Jason Calalang <jcalalang@ph.ibm.com>
2025-01-15 11:58:54 +0800

    update ssh key

--- ssh.tf
+++ ssh.tf
@@ -1,5 +1,5 @@
 # Create an SSH Key in IBM Cloud if it does not exist
 resource "ibm_is_ssh_key" "ssh-key" {
-  name       = "my-vsi-ssh"
+  name       = "${var.vpc_name}-ssh"
   public_key = var.ssh_public_key
 }
