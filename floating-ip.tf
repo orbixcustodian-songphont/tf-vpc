dc26e8589fa829a69c7175a2ce6432b7125d44cf
Jason Calalang <jcalalang@ph.ibm.com>
2025-01-18 00:47:22 +0800

    update floating ip

--- floating-ip.tf
+++ floating-ip.tf
@@ -3,3 +3,9 @@ resource "ibm_is_floating_ip" "fip1" {
   zone = "jp-tok-1"
   resource_group = data.ibm_resource_group.resource-group-name.id
 }
+
+resource "ibm_is_floating_ip" "ovpn1" {
+  name = "${var.vsi_openvpn_name}-floating-ip"
+  zone = "jp-tok-1"
+  resource_group = data.ibm_resource_group.resource-group-name.id
+}
